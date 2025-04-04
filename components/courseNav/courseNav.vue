95d88e44d93946fb6ec95407a5e20fbdc4b80efe
JiaSeng02 <jiaseng02@gmail.com>
2025-04-05 01:18:31 +0800

    MainPage complete

---
+++ components/courseNav/courseNav.vue
@@ -0,0 +1,68 @@
+<template>
+	<view class="course_nav">
+		<view class="course_nav_info" v-for="(item, index) in list" :key="index">
+			<!-- v-bind: 简写: -->
+			<text class="course_nav_icon icon iconfont" :class="item.icon"></text>
+			<view class="course_info_text">{{ item.text }}</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return{
+				list: []
+			}
+		},
+		mounted(){
+			this.list = require('@/common/data.json');
+			//console.log(this.list)
+		}
+	}
+</script>
+
+<style lang="scss">
+	@import "@/common/icofont/icofont/icofont.css";
+	.course_nav{
+		display: flex;
+		// 盒子模型不撑开容器本身大小
+		box-sizing: border-box;
+		flex-direction: row;
+		flex-wrap: wrap;
+		// 上下 左右
+		padding: 15px 10px;
+		
+		.course_nav_info{
+			width: 20%;
+			flex-direction: row;
+			flex-wrap: wrap;
+			text-align: center;
+			margin-bottom: 15px;
+			
+			.course_nav_icon {
+				font-size: 30px;
+			}
+			.icofont-brand-java {
+				color: #2a83fe;
+			}
+			.icofont-file-python {
+				color: #fd6012;
+			}
+			.icofont-web {
+				color: #00b478;
+			}
+			.icofont-data{
+				color: #2b91e2;
+			}
+			.course_info_text {
+				width: 100%;
+				font-size: 13px;
+				margin-top: 10px;
+				white-space: nowrap;
+				text-overflow: ellipsis;
+				overflow: hidden;
+			}
+		}
+	}
+</style>
--- pages/tabbar/index/index.vue
+++ pages/tabbar/index/index.vue
@@ -1,6 +1,7 @@
 <template>
 	<view class="home">
 		<Navbar />
+		
 		<view class="index_banner_box">
 			<swiper :indicator-dots="true" :autoplay="true" :interval="3000" :duration="500">
 				<swiper-item class="swiper" v-for="(item, index) in topBanner" :key="index">
@@ -8,15 +9,34 @@
 				</swiper-item>
 			</swiper>
 		</view>
+		
+		<courseNav />
+		
+		<view class="online_box" v-if="index_banner.length > 1" >
+			<image :src="index_banner[1].urls.regular"></image>
+		</view>
+		
+		<view class="free_box">
+			<view class="free_T_box public_tow_box">
+				<view class="public_T">
+					限时免费
+				</view>
+			</view>
+		</view>
+		
+		<FreeCard />
 	</view>
 </template>
 
 <script>
 	import Navbar from '@/components/navbar/navbar.vue'
+	import courseNav from '@/components/courseNav/courseNav.vue'
+	import FreeCard from '@/components/free-card/free-card.vue'
 	export default {
 		data() {
 			return {
-				topBanner: []
+				topBanner: [],
+				index_banner: [],
 			}
 		},
 		methods: {
@@ -30,6 +50,7 @@
 				success: (res) => {
 					console.log('API response:', res.data); // Debug log
 					this.topBanner = res.data;
+					this.index_banner = res.data;
 				},
 				fail: (err) => {
 					console.log('API request fail');
@@ -40,7 +61,9 @@
 			})
 		},
 		components: {
-			Navbar
+			Navbar,
+			courseNav,
+			FreeCard
 		}
 	}
 </script>
@@ -50,6 +73,7 @@
 	display: flex;
 	flex-direction: column;
 	flex: 1;
+	overflow: hidden;
 	
 	.index_banner_box {
 		width: 100%;
@@ -68,6 +92,37 @@
 				width: 100%;
 				height: 100%;
 			}
+		}
+	}
+	.online_box{
+		display: flex;
+		width: 750rpx;
+		justify-content: center;
+		align-items: center;
+		box-sizing: border-box;
+		overflow: hidden;
+		margin-bottom: 15px;
+		
+		.online_img{
+			width: 724rpx;
+			height: 132rpx;
+		}
+	}
+	.public_tow_box {
+		display: flex;
+		width: 100%;
+		justify-content: center;
+		align-items: center;
+		box-sizing: border-box;
+		overflow: hidden;
+		padding: 0 15px;
+		justify-content: space-between;
+		align-content: space-between;
+		flex-wrap: wrap;
+		
+		.public_T{
+			font-size: 20px;
+			font-weight: 700;
 		}
 	}
 }
